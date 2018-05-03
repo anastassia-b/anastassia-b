a4e04dda0b0a793d0e7e47e88b5914d75df6579a
Anastassia Bobokalonova <email.anastassia@gmail.com>
2018-05-02 20:37:44 -0700

    Move App into folder and add styles

--- src/App/App.js
+++ src/App/App.js
@@ -4,8 +4,8 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        heyo
+      <div className="app-container">
+        hey I'm Anastassia 👍
       </div>
     );
   }
